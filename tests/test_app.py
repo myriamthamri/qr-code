f1455501bd180a27d01e91b790f100dd8570f4df
myriamthamri <myriamthamri2@gmail.com>
2026-04-25 21:04:00 +0000

    Edit test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,4 +1,9 @@
-from app import app
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from app import app  # noqa: E402
 
 
 def test_home_page():
